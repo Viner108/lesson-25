6d8e19b54f593779c4996b438107da676e06bea2
Viner108 <andrukovichl@mail.ru>
2023-07-13 16:33:31 +0300

    video 2(многопоточность) commit

---
+++ src/main/java/com/dmdev/lesson25/SimpleRunnable.java
@@ -0,0 +1,8 @@
+package com.dmdev.lesson25;
+
+public class SimpleRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Hello from runnable: " + Thread.currentThread().getName());
+    }
+}
