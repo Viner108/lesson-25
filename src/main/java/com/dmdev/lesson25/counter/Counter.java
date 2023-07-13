d47abbf8483fc2f657f2314f7936c120c82bf66e
Viner108 <andrukovichl@mail.ru>
2023-07-13 19:29:14 +0300

    video 5(многопоточность) commit

--- src/main/java/com/dmdev/lesson25/counter/Counter.java
+++ src/main/java/com/dmdev/lesson25/counter/Counter.java
@@ -1,11 +1,21 @@
 package com.dmdev.lesson25.counter;
 
 public class Counter {
+    private static String description;
     private int count;
-    public void increment(){
-        count++;
+    public static void intit(){
+        synchronized (Counter.class) {
+            if (description == null) {
+                description = "Test description";
+            }
+        }
     }
-    public void decrement(){
+    public  void increment(){
+        synchronized (this){
+            count++;
+        }
+    }
+    public synchronized void decrement(){
         count--;
     }
     public int getCount(){
