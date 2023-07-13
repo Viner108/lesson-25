e95d41f02ac796f58b8dad7436d46aaf3612ac4e
Viner108 <andrukovichl@mail.ru>
2023-07-13 17:55:52 +0300

    video 4(многопоточность) commit

---
+++ src/main/java/com/dmdev/lesson25/counter/Counter.java
@@ -0,0 +1,14 @@
+package com.dmdev.lesson25.counter;
+
+public class Counter {
+    private int count;
+    public void increment(){
+        count++;
+    }
+    public void decrement(){
+        count--;
+    }
+    public int getCount(){
+        return count;
+    }
+}
