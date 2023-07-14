f6b9d6eb7f48773b8df1222059c43534b8ae896e
Viner108 <andrukovichl@mail.ru>
2023-07-14 16:21:32 +0300

    video 12(многопоточность) commit

---
+++ src/main/java/com/dmdev/lesson25/task/ConsumerThread.java
@@ -0,0 +1,33 @@
+package com.dmdev.lesson25.task;
+
+import java.util.Queue;
+
+public class ConsumerThread implements Runnable {
+    private final Queue<Integer> list;
+
+    public ConsumerThread(Queue<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        synchronized (list) {
+            while (true) {
+                if (!list.isEmpty()) {
+                    Integer removeValue = list.remove();
+                    System.out.println("consumer get value: " + removeValue + ". Size: " + list.size());
+                } else {
+                    System.out.println("consumer is waiting, List is empty");
+                }
+                try {
+                    int random = 80;
+                    System.out.println("consumer wait: " + random);
+                    list.notifyAll();
+                    list.wait(random);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/dmdev/lesson25/task/RandomUtil.java
@@ -0,0 +1,18 @@
+package com.dmdev.lesson25.task;
+
+import java.util.Random;
+
+public final class RandomUtil {
+    private static final Random RANDOM=new Random();
+    private static final int DEFAULT_BOUND=10;
+
+    public RandomUtil() {
+    }
+    public static int getRandom(){
+        return RANDOM.nextInt(DEFAULT_BOUND);
+    }
+    public static int getRandom(int bound){
+        return RANDOM.nextInt(bound);
+    }
+
+}
