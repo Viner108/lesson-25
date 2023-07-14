c8c082a8f658bbe4ef2a71dd2573b580c00eeb10
Viner108 <andrukovichl@mail.ru>
2023-07-14 14:36:20 +0300

    video 10(многопоточность) commit

--- src/main/java/com/dmdev/lesson25/queue/BuyerThread.java
+++ src/main/java/com/dmdev/lesson25/queue/BuyerThread.java
@@ -12,17 +12,20 @@ public class BuyerThread implements Runnable {
     @Override
     public void run() {
         try {
-            while (true) {
-                if (!cashboxes.isEmpty()) {
-                    Cashbox cashbox = cashboxes.remove();
-                    System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashbox);
-                    Thread.sleep(5L);
-                    System.out.println(Thread.currentThread().getName() + " освобождает кассу " + cashbox);
-                    cashboxes.add(cashbox);
-                    break;
-                } else {
-                    System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
-                    Thread.sleep(5L);
+            synchronized (cashboxes) {
+                while (true) {
+                    if (!cashboxes.isEmpty()) {
+                        Cashbox cashbox = cashboxes.remove();
+                        System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashbox);
+                        cashboxes.wait(5L);
+                        System.out.println(Thread.currentThread().getName() + " освобождает кассу " + cashbox);
+                        cashboxes.add(cashbox);
+                        cashboxes.notifyAll();
+                        break;
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
+                        cashboxes.wait();
+                    }
                 }
             }
         } catch (InterruptedException e) {
