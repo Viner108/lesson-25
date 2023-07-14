102203781f969b323c900d9ef9963131cfddce14
Viner108 <andrukovichl@mail.ru>
2023-07-14 12:54:03 +0300

    video 9(многопоточность) commit

---
+++ src/main/java/com/dmdev/lesson25/queue/Cashbox.java
@@ -0,0 +1,17 @@
+package com.dmdev.lesson25.queue;
+
+public class Cashbox {
+    private static int generator=1;
+    private int id;
+
+    public Cashbox() {
+        this.id = generator++;
+    }
+
+    @Override
+    public String toString() {
+        return "Cashbox{" +
+                "id=" + id +
+                '}';
+    }
+}
