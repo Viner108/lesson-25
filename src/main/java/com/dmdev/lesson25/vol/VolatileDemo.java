b7fb2720f970b3f7122c9350e47b1d7af9a05f71
Viner108 <andrukovichl@mail.ru>
2023-07-14 12:01:25 +0300

    video 7(многопоточность) commit

---
+++ src/main/java/com/dmdev/lesson25/vol/VolatileDemo.java
@@ -0,0 +1,19 @@
+package com.dmdev.lesson25.vol;
+
+public class VolatileDemo {
+    private static volatile boolean flag=false;
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            if (!flag) {
+                System.out.println("still false");
+            }
+        });
+        thread1.start();
+        Thread.sleep(5L);
+        Thread thread2=new Thread(()->{
+            flag=true;
+            System.out.println("flag is set");
+        });
+        thread2.start();
+    }
+}
